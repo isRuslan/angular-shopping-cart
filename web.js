1a7e101cae02a222530e9c9442e13b1688ba9b6a
Ruslan Ismagilov <is.ismagilov@gmail.com>
2014-04-15 23:39:10 +0400

    add express server

---
+++ web.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.use('/', express.static('prod/'));
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
